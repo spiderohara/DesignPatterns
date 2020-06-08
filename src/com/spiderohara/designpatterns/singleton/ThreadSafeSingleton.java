841bf6f6441160c3598e56214236c8cbeab38762
spiderohara <spiderohara44@gmail.com>
2020-06-08 16:49:15 +0530

    Thread Safe Singleton Class Added.

---
+++ src/com/spiderohara/designpatterns/singleton/ThreadSafeSingleton.java
@@ -0,0 +1,36 @@
+package com.spiderohara.designpatterns.singleton;
+
+public class ThreadSafeSingleton {
+	Object o = new Object();
+
+	// Single Instance we need to return.
+	private ThreadSafeSingleton threadSafeSingleton = null;
+
+	// Private Constructor: So that outsider won;t be able to create more instances.
+	private ThreadSafeSingleton() {
+
+	}
+
+	// Method to get the instance(single) of the class.
+	public ThreadSafeSingleton getInstance() {
+		if (threadSafeSingleton == null) {
+			synchronized (o) {
+				// Initialize the singleton class.
+				threadSafeSingleton = new ThreadSafeSingleton();
+			}
+		}
+
+		return threadSafeSingleton;
+	}
+
+	// Here we will add all the methods needed to be added.
+	public void Method(ThreadSafeSingleton threadSafeSingleton) {
+		if (this.threadSafeSingleton == threadSafeSingleton) {
+			// Both are the same references.
+			System.out.println("You have singleton instance.");
+		} else {
+			System.out.println("There is some problem with your singleton instance.");
+		}
+	}
+
+}
